57d15e3f52da1c106e604f3a299e773575ba879f
Vickee Johnson <vi436649@ucf.edu>
2020-01-29 12:22:07 -0500

    Lab 1 Progress

---
+++ ice/w4/src/Task.js
@@ -0,0 +1,16 @@
+class Task {
+    
+    constructor(props) {
+        this.props = props;
+    }
+
+    render() {
+        return `<span>
+        <input type="checkbox"></input>
+        <span>${this.props.content}</span>
+        </span>`;
+    }
+
+}
+
+export default Task;
---
+++ labs/1/Adder.js
@@ -0,0 +1,13 @@
+class Adder {
+    
+    constructor(props) {
+        this.props = props;
+    }
+
+    render() {
+        return ` `;
+    }
+
+}
+
+export default Adder;
