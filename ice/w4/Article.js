31f5ddd19ef1b51d67056770534a3dfeb22dec34
Vickee Johnson <vickeejohnson@knights.ucf.edu>
2020-02-02 21:08:00 -0500

    Completed ICE with Noah

--- ice/w4/Article.js
+++ ice/w4/Article.js
@@ -1,19 +1,16 @@
-class Article {
-
-    constructor(props) {
+class Task {
+    
+  constructor(props) {
       this.props = props;
-    }
-  
-    render() {
-      
-      return `<Article>
-        <ArticleTitle>${this.props.title}</ArticleTitle>
-        <ArticleAuthor>${this.props.author}</ArticleAuthor>
-        <ArticleText>${this.props.text}</ArticleText>
-      </Article>`;
-      
-    }
-  
   }
-  
-  module.exports = Article;
+
+  render() {
+      return `<span>
+      <input type="checkbox"></input>
+      <span>${this.props.content}</span>
+      </span>`;
+  }
+
+}
+
+export default Task;
--- ice/w4/src/index.js
+++ ice/w4/src/index.js
@@ -6,11 +6,20 @@ function runOnLoad()
     let element = document.createElement("div");
     element.id = "Test";
     document.body.appendChild(element);
-    // console.log(x);
     var comp = new Component(document.getElementById("addTask"));
     let element2 = document.createElement("div");
     element2.innerHTML = comp.render();
     console.log("Hello World truly");
+    let myTask = new Task(
+        {
+            content: "Finish ICE Exercise", 
+            done: false
+        }
+    )
+    var container = document.getElementById("taskList");
+    var newtask = document.createElement("div");
+    newtask.innerHTML = myTask.render();
+    container.appendChild(newtask);
 }
 
 window.addEventListener("DOMContentLoaded", runOnLoad);
