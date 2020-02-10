784dfa3b8381b849efbf085c8ef81df1d7969d24
Vickee Johnson <vi436649@ucf.edu>
2020-02-10 12:23:45 -0500

    #95 Begin Tic Tac Toe

--- demo/hello_world/src/App.js
+++ demo/hello_world/src/App.js
@@ -2,13 +2,25 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function NameBadge(props) {
+  console.log(props);
+  return (
+    <p>My Name is {props.name}</p>)
+}
+
+class App extends React.Component {
+  clickHandler = () => {
+    alert("Clicked");
+  }
+  render() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} onClick={this.clickHandler()} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello World,
+          <NameBadge name="Vickee" />
+          <NameBadge name="Johnson" />
         </p>
         <a
           className="App-link"
@@ -21,6 +33,7 @@ function App() {
       </header>
     </div>
   );
+  }
 }
 
 export default App;
