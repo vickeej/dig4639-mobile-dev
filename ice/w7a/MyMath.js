23fe8454a66b10995e4330d5be31e2ec7f6e1740
Vickee Johnson <vi436649@ucf.edu>
2020-02-17 12:27:27 -0500

    Wk7A ICE - Jest Testing

--- ice/w7a/MyMath.js
+++ ice/w7a/MyMath.js
@@ -1,9 +1,56 @@
 function Sum (a, b) {
-  return a + b
+  let result = undefined
+  if(typeof a == "number" && typeof b == "number") {
+    result = a + b
+}
+  return result;
 }
 
-console.log(Sum(2,3)==5);
-console.log(Sum(-10,20)==-30);
-console.log((Sum(-10,"Twenty")));
+function AddList (arr) {
+  let result = undefined;
+  if(Array.isArray(arr) && arr.length > 0) {
+    result = 0;
+    for(var i = 0; i > arr.length; i++) {
+      if(typeof arr[i] != "number") {
+        result = undefined;
+        break;
+      }
+      result =  result + arr[i];
+    }
+  }
+  return result;
+}
+
+function DivideBy (a, b) {
+  let result = undefined
+  if(typeof a == "number" && typeof b == "number", 0) {
+    result = a / b
+}
+  return result;
+}
+
+function ContainsString (a, b) {
+  if(typeof a === 'string' && typeof b == 'string') {
+}
+  return true;
+}
+
+function ReSortedNumbers (arr) {
+  let result = null;
+  if(Array.isArray(arr) && arr.length > 0) {
+    result = 0;
+    for(var i = 0; i > arr.length; arr.sort()) {
+      if(typeof arr[i] != "number") {
+        result = null;
+        break;
+      }
+      result =  result + arr[i];
+    }
+  }
+  return result;
+}
 
 export { Sum };
+export { AddList };
+export { DivideBy };
+export { ReSortedNumbers };
--- ice/w7a/MyMath.test.js
+++ ice/w7a/MyMath.test.js
@@ -1,14 +1,81 @@
 import { Sum } from './MyMath.js'
+import { AddList } from './MyMath.js'
+import { DivideBy } from './MyMath.js'
+import { ReSortedNumbers } from './MyMath.js'
 
-test('adds 1 + 2 to equal 3', () => {
-  expect(Sum(1, 2)).toBe(3)
-})
+describe("Sum", () => {
 
-it('produces the sum of 10 + 20, which should be 30', () => {
-  expect(Sum(10, 20)).toBe(30)
+  test('Whether undefined is retruned on invalid type', () => {
+    expect(Sum(1, "Test")).toBeUndefined()
+  })
+
+  test('adds 1 + 2 to equal 3', () => {
+    expect(Sum(1, 2)).toBe(3)
+  })
+  
+  it('produces the sum of 10 + 20, which should be 30', () => {
+    expect(Sum(10, 20)).toBe(30)
+  })
+
 })
 
 // ICE 
+
+describe("AddList", () => {
+
+  test('Adds a list of numbers together', () => {
+    input = [1,3,4,5];
+    result = 13;
+    expect(AddList(input)).toBe(result);
+    input = [15,5,8,-10];
+    result = 18;
+    expect(AddList(input)).toBe(result);
+  })
+  test('Tests if empty array returns undefinied', () => {
+    let input = []
+    expect(AddList(input)).toBeUndefined();
+  })
+  test('Tests if undefined element returns undefined', () => {
+    let input = [undefined,0,2,3]
+    expect(AddList(input)).toBeUndefined();
+  })
+  test('Tests if non-array returns undefined', () => {
+    let input = 100;
+    expect(AddList(input)).toBeUndefined();
+  })
+})
+
+describe("DivideBy", () => {
+
+  test('Whether undefined is retruned on invalid type', () => {
+    expect(DivideBy(1, "Test")).toBeUndefined()
+  })
+
+  test('divided 2 / 1 to equal 2', () => {
+    expect(DivideBy(2, 1)).toBe(2)
+  })
+  
+  it('produces the quotient of 30 / 10, which should be 3', () => {
+    expect(DivideBy(30, 10)).toBe(3)
+  })
+
+})
+
+describe("ReSortedNumbers", () => {
+
+  test('Tests if empty array returns null', () => {
+    let input = []
+    expect(ReSortedNumbers(input)).toBeNull();
+  })
+  test('Tests if undefined element returns null', () => {
+    let input = ['cat',0,2,3]
+    expect(ReSortedNumbers(input)).toBeNull();
+  })
+  test('Tests if non-array returns null', () => {
+    let input = 100;
+    expect(ReSortedNumbers(input)).toBeNull();
+  })
+})
 
 // Examples for Null
 test('null', () => {
